a765ce3b481b21c9e294c2490b5f78719e53597e
Ashik Rahman <ashrhmn99@gmail.com>
2021-04-25 09:04:12 +0600

    added gitignore

--- dist/assets/ItemDataService.c010ee4d.js
+++
@@ -1,1 +0,0 @@
-import{h as e}from"./index.06ab4907.js";var t=new class{getAll(){return e.get("/items")}get(t){return e.get(`/items/${t}`)}create(t){return e.post("/items",t)}update(t,r){return e.put(`/items/${t}`,r)}delete(t){return e.delete(`/items/${t}`)}};export{t as I};
